6578902a15c78146728b4bdb32644f7c7b9f0b86
FabiolaTP <tapiafaby3@gmail.com>
2020-02-26 22:27:38 -0600

    valida, lee y encuentra links de un archivo .md

---
+++ index.js
@@ -0,0 +1,33 @@
+//uso obligartorio/strict use
+const fs = require('fs');
+const path = require('path');
+const file = process.argv[2];
+
+//validando si el archivo es .md/validate if the file is .md
+const validateMD = () => {
+    let foundExt = path.extname(file);
+    if (foundExt === '.md') {
+        console.log('This is a .md file'); 
+        return true; 
+    } 
+    else {
+        console.log('This isn\'t a .md file');
+        return false;    
+    }    
+}
+console.log(validateMD(file));
+
+//
+const readFileMd = fs.readFile( file, "utf8", (err, data) => {
+    if (validateMD(file) === false) {
+      console.log(err);
+    } 
+    else {
+      const findLink = new RegExp(/https?:\/\/[\w\.\-]+\.\w{2,5}[^\s\)]+/g);
+      const links = data.match(findLink);
+      console.log(links);
+    }
+});
+console.log(readFileMd);
+
+//Continuará.../To be continue..... ;)
